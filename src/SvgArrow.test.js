f37a3a9351e39ec99028f6b4253f9b995080ce06
Pierre Poupin <pierrep@theodo.fr>
2018-03-17 12:39:48 +0100

    Add more complete tests of SvgArrow

--- src/SvgArrow.test.js
+++ src/SvgArrow.test.js
@@ -1,4 +1,10 @@
-import { computeEndingPointAccordingToArrow } from './SvgArrow';
+import React from 'react';
+import { shallow } from 'enzyme';
+import SvgArrow, {
+  computeEndingPointAccordingToArrow,
+  computeStartingAnchorPosition,
+  computeEndingAnchorPosition,
+} from './SvgArrow';
 
 describe('SvgArrow', () => {
   describe('computeEndingPointAccordingToArrow', () => {
@@ -50,7 +56,9 @@ describe('SvgArrow', () => {
     ];
 
     dataSet.forEach(data => {
-      it(`should return proper result for dataset ${data.message}`, () => {
+      it(`should compute coordinates of destination point excluding the arrow [data ${
+        data.message
+      }]`, () => {
         const {
           xEnd,
           yEnd,
@@ -66,6 +74,126 @@ describe('SvgArrow', () => {
           endingAnchor,
         );
         expect(result).toEqual(data.expected);
+      });
+    });
+  });
+
+  describe('computeStartingAnchorPosition', () => {
+    const dataSet = [
+      {
+        message: 1,
+        input: {
+          xStart: 10,
+          yStart: 20,
+          xEnd: 5,
+          yEnd: 2,
+          endingAnchor: 'top',
+        },
+        expected: { xa1: 10, ya1: 11 },
+      },
+      {
+        message: 2,
+        input: {
+          xStart: 10,
+          yStart: 20,
+          xEnd: 5,
+          yEnd: 2,
+          endingAnchor: 'left',
+        },
+        expected: { xa1: 7.5, ya1: 20 },
+      },
+    ];
+
+    dataSet.forEach(data => {
+      it(`should compute coordinates of source element's anchor [data ${
+        data.message
+      }]`, () => {
+        const { xStart, yStart, xEnd, yEnd, endingAnchor } = data.input;
+        const result = computeStartingAnchorPosition(
+          xStart,
+          yStart,
+          xEnd,
+          yEnd,
+          endingAnchor,
+        );
+        expect(result).toEqual(data.expected);
+      });
+    });
+  });
+
+  describe('computeEndingAnchorPosition', () => {
+    const dataSet = [
+      {
+        message: 1,
+        input: {
+          xStart: 10,
+          yStart: 20,
+          xEnd: 5,
+          yEnd: 2,
+          endingAnchor: 'top',
+        },
+        expected: { xa2: 5, ya2: 11 },
+      },
+      {
+        message: 2,
+        input: {
+          xStart: 10,
+          yStart: 20,
+          xEnd: 5,
+          yEnd: 2,
+          endingAnchor: 'left',
+        },
+        expected: { xa2: 7.5, ya2: 2 },
+      },
+    ];
+
+    dataSet.forEach(data => {
+      it(`should compute coordinates of destination element's anchor [data ${
+        data.message
+      }]`, () => {
+        const { xStart, yStart, xEnd, yEnd, endingAnchor } = data.input;
+        const result = computeEndingAnchorPosition(
+          xStart,
+          yStart,
+          xEnd,
+          yEnd,
+          endingAnchor,
+        );
+        expect(result).toEqual(data.expected);
+      });
+    });
+  });
+
+  describe('component', () => {
+    let wrapper;
+
+    const props = {
+      startingPoint: { x: 10, y: 10 },
+      startingAnchor: 'bottom',
+      endingPoint: { x: 30, y: 30 },
+      endingAnchor: 'top',
+      strokeColor: 'blue',
+      arrowLength: 10,
+      strokeWidth: 2,
+    };
+
+    beforeEach(() => {
+      wrapper = shallow(<SvgArrow {...props} />);
+    });
+
+    it('should render path with proper coordinates', () => {
+      // The functions should be mocked otherwise the test is not a unit test
+      // But they need to become component instance functions
+      // For now, let's not mock them
+      const path = wrapper.find('path');
+
+      expect(path.props()).toMatchObject({
+        d: 'M10,10 C10,10 30,10 30,10',
+        markerEnd: 'url(#arrow)',
+        style: {
+          strokeWidth: 2,
+          stroke: 'blue',
+        },
       });
     });
   });
