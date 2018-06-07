6b074de3d41f40671ccdda22ebf3448b154e4095
joao.ferreira <joao.ferreira@corp.badoo.com>
2018-06-07 16:14:25 +0100

    Updating example App

--- example/App.js
+++ example/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ArcherContainer from '../src/ArcherContainer';
-import ArcherElement from '../src/ArcherElement'
+import ArcherElement from '../src/ArcherElement';
 
 const rootStyle = { display: 'flex', justifyContent: 'center' };
-const rowStyle = { margin: '200px 0', display: 'flex', justifyContent: 'space-between' };
+const rowStyle = {
+  margin: '200px 0',
+  display: 'flex',
+  justifyContent: 'space-between',
+};
 const boxStyle = { padding: '10px', border: '1px solid black' };
 
 const App = () => {
   return (
     <div style={{ height: '500px', margin: '50px' }}>
-
-      <ArcherContainer strokeColor='red' >
+      <ArcherContainer strokeColor="red">
         <div style={rootStyle}>
           <ArcherElement
             id="root"
-            relations={[{
-              from: { anchor: 'bottom' },
-              to: { anchor: 'top', id: 'element2' }
-            }]}
+            relations={[
+              {
+                from: { anchor: 'bottom' },
+                to: { anchor: 'top', id: 'element2' },
+              },
+            ]}
           >
             <div style={boxStyle}>Root</div>
           </ArcherElement>
@@ -26,10 +31,13 @@ const App = () => {
         <div style={rowStyle}>
           <ArcherElement
             id="element2"
-            relations={[{
-              from: { anchor: 'right'},
-              to: { anchor: 'left', id: 'element3' }
-            }]}
+            relations={[
+              {
+                from: { anchor: 'right' },
+                to: { anchor: 'left', id: 'element3' },
+                label: <div style={{ marginTop: '-20px' }}>Arrow 2</div>,
+              },
+            ]}
           >
             <div style={boxStyle}>Element 2</div>
           </ArcherElement>
@@ -40,18 +48,20 @@ const App = () => {
 
           <ArcherElement
             id="element4"
-            relations={[{
-              from: { anchor: 'left'},
-              to: { anchor: 'right', id: 'root' }
-            }]}
+            relations={[
+              {
+                from: { anchor: 'left' },
+                to: { anchor: 'right', id: 'root' },
+                label: 'Arrow 3',
+              },
+            ]}
           >
             <div style={boxStyle}>Element 4</div>
           </ArcherElement>
         </div>
       </ArcherContainer>
-
     </div>
   );
-}
+};
 
 export default App;
