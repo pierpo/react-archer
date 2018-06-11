57b11dc03e8200125a96e9b197f7dbc79eedfd3e
Pierre Poupin <pierrep@theodo.fr>
2018-06-11 20:09:07 +0200

    Export (incorrect) logic of ArcherElement relations prop stringification to function

--- src/ArcherElement.js
+++ src/ArcherElement.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const stringifyRelations = relations => {
+  const relationsWithStringifiedLabels = (relations || []).map(r => {
+    return (r.label || {}).toString();
+  });
+  return JSON.stringify(relationsWithStringifiedLabels);
+};
+
 export class ArcherElement extends React.Component {
   componentWillReceiveProps(nextProps) {
-    const currentRelations = (this.props.relations || []).map(r =>
-      (r.label || {}).toString(),
-    );
-    const nextRelations = (nextProps.relations || []).map(r =>
-      (r.label || {}).toString(),
-    );
-    if (JSON.stringify(currentRelations) === JSON.stringify(nextRelations)) {
+    if (
+      stringifyRelations(this.props.relations) ===
+      stringifyRelations(nextProps.relations)
+    ) {
       return;
     }
     this.registerAllTransitions(nextProps.relations);
