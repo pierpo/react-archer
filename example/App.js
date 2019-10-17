594b5c5a2507d6524925c68c34ade1983e54b4ea
Pierre Poupin <pierrep@bam.tech>
2019-10-17 23:58:44 +0200

    add fourth example

--- example/App.js
+++ example/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FirstExample from './FirstExample';
 import SecondExample from './SecondExample';
 import ThirdExample from './ThirdExample';
+import FourthExample from './FourthExample';
 
 class App extends React.Component {
   state = { currentExample: 1 };
@@ -14,6 +15,8 @@ class App extends React.Component {
         return SecondExample;
       case 3:
         return ThirdExample;
+      case 4:
+        return FourthExample;
       default:
         return SecondExample;
     }
@@ -33,6 +36,7 @@ class App extends React.Component {
           <button onClick={this.changeExample(1)}>Example 1</button>
           <button onClick={this.changeExample(2)}>Example 2</button>
           <button onClick={this.changeExample(3)}>Example 3</button>
+          <button onClick={this.changeExample(4)}>Example 4</button>
         </div>
         <hr />
         <Example />
