5692942c36d9c634955052d0f8f80551fdf03b5a
Pierre Poupin <pierrep@bam.tech>
2020-11-19 00:10:13 +0100

    refactor: use hook for second example

--- example/SecondExample.js
+++ example/SecondExample.js
@@ -10,75 +10,63 @@ const rowStyle = {
 };
 const boxStyle = { padding: '10px', border: '1px solid black' };
 
-class SecondExample extends React.Component {
-  state = { nbElements: 3, labels: 'hello' };
+const SecondExample = () => {
+  const [nbElements, setNbElements] = React.useState(3);
+  const [labels, setLabels] = React.useState('hello');
 
-  render = () => {
-    return (
-      <div style={{ height: '500px', margin: '50px' }}>
-        <div>
-          <div>Change labels</div>
-          <input
-            data-cy="change-labels-input"
-            type="text"
-            onChange={event => {
-              this.setState({ labels: event.currentTarget.value });
-            }}
-          />
-        </div>
-        <div>
-          <div>Add elements</div>
-          <button
-            data-cy="add-element"
-            onClick={() => this.setState({ nbElements: this.state.nbElements + 1 })}
-          >
-            +
-          </button>
-          <button
-            onClick={() =>
-              this.setState({
-                nbElements: this.state.nbElements > 1 ? this.state.nbElements - 1 : 0,
-              })
-            }
-          >
-            -
-          </button>
-        </div>
-        <ArcherContainer strokeColor="red">
-          <div style={rootStyle}>
-            <ArcherElement id="root">
-              <div style={boxStyle}>Root</div>
-            </ArcherElement>
-          </div>
-
-          <div style={rowStyle}>
-            {Array(this.state.nbElements)
-              .fill()
-              .map((_, i) => (
-                <ArcherElement
-                  key={`element${i}`}
-                  id={`element${i}`}
-                  relations={[
-                    {
-                      targetId: 'root',
-                      targetAnchor: 'bottom',
-                      sourceAnchor: 'top',
-                      label: (
-                        <div>
-                          {i} {this.state.labels}
-                        </div>
-                      ),
-                    },
-                  ]}
-                >
-                  <div style={boxStyle}>Element {i}</div>
-                </ArcherElement>
-              ))}
-          </div>
-        </ArcherContainer>
+  return (
+    <div style={{ height: '500px', margin: '50px' }}>
+      <div>
+        <div>Change labels</div>
+        <input
+          data-cy="change-labels-input"
+          type="text"
+          onChange={event => {
+            setLabels(event.currentTarget.value);
+          }}
+        />
       </div>
-    );
-  };
-}
+      <div>
+        <div>Add elements</div>
+        <button data-cy="add-element" onClick={() => setNbElements(nbElements + 1)}>
+          +
+        </button>
+        <button onClick={() => setNbElements(nbElements > 1 ? nbElements - 1 : 0)}>-</button>
+      </div>
+      <ArcherContainer strokeColor="red">
+        <div style={rootStyle}>
+          <ArcherElement id="root">
+            <div style={boxStyle}>Root</div>
+          </ArcherElement>
+        </div>
+
+        <div style={rowStyle}>
+          {Array(nbElements)
+            .fill()
+            .map((_, i) => (
+              <ArcherElement
+                key={`element${i}`}
+                id={`element${i}`}
+                relations={[
+                  {
+                    targetId: 'root',
+                    targetAnchor: 'bottom',
+                    sourceAnchor: 'top',
+                    label: (
+                      <div>
+                        {i} {labels}
+                      </div>
+                    ),
+                  },
+                ]}
+              >
+                <div style={boxStyle}>Element {i}</div>
+              </ArcherElement>
+            ))}
+        </div>
+      </ArcherContainer>
+    </div>
+  );
+};
 
 export default SecondExample;
