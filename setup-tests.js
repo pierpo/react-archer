4617588de9ebe75f36d50caae6993d1f25ad2749
Pierre Poupin <pierrep@bam.tech>
2022-04-02 04:21:41 +0200

    chore: setup testing-library

--- setup-tests.js
+++ setup-tests.js
@@ -1,5 +1,7 @@
 import Enzyme from 'enzyme';
+import 'regenerator-runtime/runtime';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jsdom-global/register';
+import 'window-resizeto/polyfill';
 
 Enzyme.configure({ adapter: new Adapter() });
