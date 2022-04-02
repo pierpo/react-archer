e1962e9df92502b0a37bf53d4934ef55e8d50951
Pierre Poupin <pierrep@bam.tech>
2022-04-02 05:17:52 +0200

    chore: reorganize jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   roots: ['src/'],
   setupFilesAfterEnv: ['./jest.setup.after.js'],
-  setupFiles: ['./shim.js', './setup-tests.js'],
+  setupFiles: ['./jest.setup.js'],
 };
