07c3c0b721070b5bce1d50a627edd5d2146b42a7
Pierre Poupin <pierrep@bam.tech>
2022-11-04 00:40:56 +0100

    chore(cypress): uprade cypress to latest

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  video: false,
+  watchForFileChanges: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:8080',
+  },
+});
